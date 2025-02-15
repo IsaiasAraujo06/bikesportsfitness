17c5ffe1c6824a7ab37a2ef7b18c221ede30235b
Isaías Fernandes <fernandesisaias886@gmail.com>
2025-02-15 11:16:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,186 @@
+const categorias = {
+    esteiras:[
+        {nome: "ESTEIRA EVOLUTUION 4000 PRO C/INCLINAÇÃO", imagem:"assets/esteiras/evo4000proincl.jpg", preco: "R$15389,94", whatsapp: "+5586988946118"},
+        {nome: "ESTEIRA MOVEMENT LX-150 G4", imagem: "assets/esteiras/lx-150.jpg", preco: "R$14990,00", whatsapp: "+5586988946118"},
+        {nome: "ESTEIRA MOVEMENT LX-160 G4", imagem: "assets/esteiras/lx-160.jpg", preco: "R$16497,89", whatsapp: "+5586988946118"},
+        {nome: "ESTEIRA MOVEMENT RT-150 LED", imagem: "assets/esteiras/rt-150.jpg", preco: "R$23339,76", whatsapp: "+5586988946118"},
+
+    ],
+
+    bicicletas: [
+        {nome: "AUDAX HAVOK NX T19 PRETO AZUL", imagem: "assets/bikes/pretoazul.jpg", preco: "R$2.421,57", whatsapp: "+5586988946118"},
+        {nome: "AUDAX HAVOK NX T19 VERMELHA", imagem: "assets/bikes/vermelha.jpg", preco: "R$3.748,32", whatsapp: "+5586988941168"},
+        {nome: "AUDAX HAVOK NX T19 PRETO/CINZA", imagem: "assets/bikes/pretocinza.jpg", preco: "R$3.023,74", whatsapp: "+5586988941116"},
+        {nome: "AUDAX HAVOK NX T19 PRETO/AMARELO", imagem: "assets/bikes/pretoamarelo.jpg", preco: "R$2.867,88", whatsapp: "+5586988941116"},
+        {nome: "CAPACETE ABSOLUTE", imagem: "assets/bikes/capaceteabsolute.jpg", preco: "A PARTIR DE R$113,91", whatsapp: "+5586988946116"},
+        {nome: "FAROL ABSOLUTE JY-7043 PRETO C/LED", imagem: "assets/bikes/faroljy-7043.jpg", preco: "R$119,31", whatsapp: "+5586988941116"},
+        {nome: "GARRAFA ISOTERM", imagem: "assets/bikes/garrafaisoterm.jpg", preco: "A PARTIR DE R$14,90", whatsapp: "+5586988941116"},
+        {nome: "LUVA ABSOLUTE", imagem: "assets/bikes/luvaabsolute.jpg", preco: "A PARTIR DE R$42,29", whatsapp: "+5586988941116"},
+        {nome:"TRANSBIKE ABSOLUTE 2 BIKES", imagem: "assets/bikes/transbikeabsolute.jpg", preco: "A PARTIR DE R$341,25", whatsapp: "+5586988941116"},
+
+    ],
+
+
+    ergometricas:[
+     {nome:"ERGOMÉTRICA HORIZONTAL EVOLUTION RB-802", imagem:"assets/ergometricas/rb-802.jpg", preco:"R$3796,73", whatsapp: "+5586988946118"},
+     {nome: "ERGOMÉTRICA O'NEAL HORIZONTAL TP-940", imagem: "assets/ergometricas/tp-940.jpg", preco: "R$7893,89", whatsapp: "+5586988946118"},
+     {nome: "ERGOMÉTRICA EVOLUTION SPINNING SP-2500", imagem: "assets/ergometricas/sp-2500.jpg", preco: "R$3975,00", whatsapp: "+5586988946118"},
+     {nome: "ERGOMÉTRICA EVOLUTION SPINNING SP-2600", imagem: "assets/ergometricas/sp-2600.jpg", preco: "R$5490,89", whatsapp: "+5586988946118"},
+     {nome: "ERGOMÉTRICA O'NEAL VERTICAL TP-9516", imagem: "assets/ergometricas/tp-9516.jpg", preco: "R$3249,26", whatsapp: "+5586988946118"},
+     {nome: "ERGOMÉTRICA O'NEAL VERTICAL TP-967", imagem: "assets/ergometricas/tp-967.jpg", preco: "R$3979,01", whatsapp: "+5586988946118"},
+     
+
+    ],
+
+    estacao: [
+        {nome: "ESTAÇÃO MOVEMENT W4", imagem: "assets/estacao/w4.jpg", preco: "R$15968,35", whatsapp: "+5586988946118"},
+        {nome: "ESTAÇÃO MOVEMENT W8", imagem: "assets/estacao/w8.jpg", preco: "R$20576,80", whatsapp: "+5586988946118"},
+        {nome: "ESTAÇÃO EVOLUTION FT-9000", imagem: "assets/estacao/ft-9000.png", preco: "R$6958,58", whatsapp: "+5586988946118"},
+        {nome: "ESTAÇÃO EVOLUTION FT-13000", imagem: "assets/estacao/ft-13000.jpg", preco: "R$12349,70", whatsapp: "+5586988946118"},
+    ],
+
+    maquinas: [
+        {nome: "FLY VOADOR MACSPORT", imagem: "assets/maquinas/flymacsport", preco: "R$11539,83", whatsapp: "+5586988946118"},
+        {nome: "LEGPRESS 45 MACSPORT", imagem: "assets/maquinas/leg45macsport.jpg", preco: "R$13602,66", whatsapp: "+5586988946118"},
+        
+    ],
+
+    pesolivre: [
+        {nome: "BANCO REGULÁVEL 3X1 PRO MAC SPORT", imagem: "assets/pesolivre/bancomacsport.jpg", preco: "R$2228,80", whatsapp:"+5586988946118"},
+        {nome: "MUSCULAÇÃO BOLT SUPINO AJUSTÁVEL", imagem: "assets/pesolivre/supinobolt.jpg", preco: "R$11478,50", whatsapp: "+5586988946118"},
+        {nome: "BANCO SUPINO 3 EM 1 VITAMEC", imagem: "assets/pesolivre/supinovitamec.jpg", preco: "R$908,42", whatsapp: "+5586988946118"},
+        {nome: "KIT DUMBELL TEXTURIZADO 12KG-20KG UNB", imagem: "assets/pesolivre/dumbellsUNB.jpg", preco: "R$8587,38", whatsapp: "+5586988946118"},
+        {nome: "KIT HALTERES FERRO 1KG-10KG VITAMEC", imagem: "assets/pesolivre/halteresferrovitamec.jpg", preco: "R$2364,92", whatsapp:"+5586988946118"},
+        {nome: "KIT HALTERES BORRACHA 1KG-10KG UNB", imagem: "assets/pesolivre/halteresborrachaunb.jpg", preço: "R$3068,92", whatsapp: "+5586988946118"},
+        {nome: "BARRA HEXAGONAL MACIÇA UNB", imagem: "assets/pesolivre/barrahexagonal.jpg", preco: "R$1034,11", whatsapp: "+5586988946118"},
+        {nome: "BARRA OCA VITA 1.5M CROMADA", imagem: "assets/pesolivre/barraocavita.jpg", preco: "R$186,26", whatsapp: "+5586988946118"},
+        {nome: "BARRA MACIÇA UNB 1.5M CROMADA", imagem: "assets/pesolivre/barramaciçaunb.jpg", preco: "R$401,18", whatsapp: "+5586988946118"},
+        {nome: "BARRA OLÍMPICA 1,80M UNB", imagem: "assets/pesolivre/barraolimpicaunb.jpg", preco: "R$1152,16", whatsapp: "+5586988946118"}
+    ],
+
+    acessorios: [
+        {nome: "KIT PUXADORES ANATÔMICOS UNB", imagem: "assets/acessorios/kitpuxadoresergonomicos.jpg", preco: "R$1809,74", whatsapp: "+5586988946118"},
+        {nome: "PAR DE MANOPLAS CROSSOVER UNB", imagem: "assets/acessorios/manoplasunb.jpg", preco: "R$77,41", whatsapp: "+5586988941168"},
+        {nome: "TORNOZELEIRA CROSSOVER MOVEMENT", imagem: "assets/acessorios/tornozeleiramovement.jpg", preco: "R$133,78", whatsapp: "+5586988941116"},
+        {nome: "SUPORTE PARA AGACHAMENTO", imagem: "assets/acessorios/suporteagachamento.jpg", preco: "R$279,93", whatsapp: "+5586988941116"},
+        {nome: "PUXADOR TRICEPS 50CM MOVEMENT", imagem: "assets/acessorios/puxador50cmmovement.jpg", preco: "R$299,80", whatsapp: "+5586988946116"},
+        {nome: "PUXADOR ROMANO ANGULADO 38CM", imagem: "assets/acessorios/puxadorromanoangulado.jpg", preco: "R$258,18", whatsapp: "+5586988941116"},
+        {nome: "PUXADOR TRIÂNGULO UNB", imagem: "assets/acessorios/puxadortriangulounb.jpg", preco: "R$150,00", whatsapp: "+5586988941116"},
+        {nome: "STEP ARKTUS 10CM", imagem: "assets/acessorios/steparktus.jpg", preco: "R$410,80", whatsapp: "+5586988941116"},
+        {nome: "COLCHONETE LEIRIMAX", imagem: "assets/acessorios/colchoneteleirimax.jpg", preco: "A PARTIR DE R$95,90", whatsapp:"+5586988941116"},
+        {nome:"PUXADOR CORDA MOVEMENT", imagem: "assets/acessorios/cordamovement.jpg", preco: "R$159,95", whatsapp: "+5586988941116"}
+
+    ],
+
+    
+};
+
+function adicionarProdutos(categoria, id) {
+    const secao = document.querySelector(`#${id} .catalogo`);
+    
+    categorias[categoria].forEach(item => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="${item.imagem}" alt="${item.nome}">
+            <h2>${item.nome}</h2>
+            <p>${item.preco}</p>
+            <a href="https://wa.me/${item.whatsapp}" class="btn-whatsapp" target="_blank">Comprar no WhatsApp</a>
+        `;
+
+        secao.appendChild(card);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+adicionarProdutos("esteiras", "esteiras");
+adicionarProdutos("ergometricas", "ergometricas");
+adicionarProdutos("estacao", "estacao");
+adicionarProdutos("pesolivre","pesolivre");
+adicionarProdutos("acessorios","acessorios");
+adicionarProdutos("maquinas","maquinas");
+adicionarProdutos("bicicletas","bicicletas");
+
+    const imagens = document.querySelectorAll(".catalogo img");
+    const overlay = document.getElementById("overlay");
+    const imagemExpandida = document.getElementById("imagemExpandida");
+
+    imagens.forEach(img => {
+        img.addEventListener("click", function () {
+            imagemExpandida.src = this.src;
+            overlay.classList.add("active");
+        });
+    });
+
+    overlay.addEventListener("click", function () {
+        overlay.classList.remove("active");
+    });
+});
+
+function irParaCatalogo() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("catalogo").style.display = "block";
+}
+
+function filtrarProdutos() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let catalogo = document.getElementById("catalogo");
+    let lista = document.getElementById("listaProdutos").getElementsByTagName("li");
+    let resultadosDiv = document.getElementById("resultadosPesquisa");
+  
+    resultadosDiv.innerHTML = "";
+  
+    if (input === "") {
+      catalogo.style.display = "block"; 
+      return;
+    }
+  
+    let resultados = [];
+  
+    for (let item of lista) {
+      if (item.textContent.toLowerCase().includes(input)) {
+        resultados.push(item.textContent);
+      }
+    }
+  
+    if (resultados.length > 0) {
+      catalogo.style.display = "none"; 
+      let ul = document.createElement("ul");
+      resultados.forEach((produto) => {
+        let li = document.createElement("li");
+        li.textContent = produto;
+        ul.appendChild(li);
+      });
+      resultadosDiv.appendChild(ul);
+    } else {
+      catalogo.style.display = "block";
+    }
+  }
+  
+
+function buscarProdutos(event) {
+    if (event && event.key === "Enter") {
+        event.preventDefault();
+    }
+}
+
+    const termoBusca = document.getElementById("search-input").value.toLowerCase();
+    const produtos = document.querySelectorAll(".card");
+    let encontrou = false;
+
+    produtos.forEach(produto => {
+        const nomeProduto = produto.querySelector("h2").textContent.toLowerCase();
+
+        if (nomeProduto.includes(termoBusca)) {
+            produto.style.display = "block";
+            encontrou = true;
+
+            const regex = new RegExp(`(${termoBusca})`, "gi");
+            produto.querySelector("h2").innerHTML = nomeProduto.replace(regex, "<mark>$1</mark>");
+        } else {
+            produto.style.display = "none";
+        }
+    });
+
+    document.getElementById("mensagem-erro").style.display = encontrou ? "none" : "block";
